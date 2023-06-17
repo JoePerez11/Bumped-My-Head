7f25cf301afdbd29c1a2a767fbf254df2c8c00ef
joeperez11 <perez.joseph.k@gmail.com>
2023-06-17 15:55:16 -0500

    First Commit Test

---
+++ bmh/src/main/java/com/bmh/bmh/BmhApplication.java
@@ -0,0 +1,13 @@
+package com.bmh.bmh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmhApplication.class, args);
+	}
+
+}
---
+++ bmh/src/test/java/com/bmh/bmh/BmhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmh.bmh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
